e4bc3d8f35b7bc76c727f3de004b51c2305262b5
VuDuc1610 <90380300+VuDuc1610@users.noreply.github.com>
2024-01-13 20:28:34 -0500

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import example from './example.jpg';
+
+export {
+  download,
+  logo,
+  preview,
+  example,
+};
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import Post from '../mongodb/models/post.js';
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name:"dlldciyyt",
+  api_key: "164381715454199",
+  api_secret: "xFcktTtBcm9yZQcMt82Gr-46LZ4",
+});
+
+//GET ALL POSTS 
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
+
+//CREATE NEW POST
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,   
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
+});
+
+export default router;
